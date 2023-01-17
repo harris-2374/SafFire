e79f3584a1b724bea78ddc14dcd1ead496bcdf45
harris-2374 <41405364+harris-2374@users.noreply.github.com>
2023-01-17 10:02:41 -0600

    init commit
    Added my own data sets + tweaked a few things in the index.html file

--- lib.js
+++ lib.js
@@ -339,16 +339,14 @@ function read_in_bed9_defaults() {
     zoom_bed_9 = {};
     var bed_files = {
         ref: [
-            `datasets/${REF}/${REF}_CenSat.bed`,
             `datasets/${REF}/${REF}_dupmasker_colors.bed`,
-            `datasets/${REF}/${REF}_genes_small.bed`,
             `datasets/${REF}/${REF}_gaps.bed`,
-            `datasets/${REF}/CHM1_raw.bed9`,
+            `datasets/${REF}/${REF}_invs.bed`,
         ],
         query: [
             `datasets/${QUERY}/${QUERY}_dupmasker_colors.bed`,
             `datasets/${QUERY}/${QUERY}_gaps.bed`,
-            `datasets/${QUERY}/${QUERY}_CenSat.bed`,
+            `datasets/${QUERY}/${QUERY}_invs.bed`,
         ]
     }
     for (const key in bed_files) {
